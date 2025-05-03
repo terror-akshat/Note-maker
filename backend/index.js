3dade9a7f3105f3c9cf8b0ecadbe04cb35557a15
terror-akshat <www.akshatagarwal123klj@gmail.com>
2025-05-03 11:28:22 +0530

    update

--- backend/index.js
+++ backend/index.js
@@ -14,7 +14,7 @@ app.use(cookieParser());
 app.use(express.json());
 app.use(
   cors({
-    origin: "http://localhost:5173",
+    origin: "https://curious-profiterole-8d459b.netlify.app",
     methods: ["GET", "POST", "PUT", "DELETE"],
     credentials: true,
   })
